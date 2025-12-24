7013926335353c8b3d2e4cd3efa578a19b971ed8
Abdulrahman <syamabdo382@gmail.com>
2025-12-24 22:01:54 +0200

    navigate to home view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets_data.dart';
-import 'package:bookly/features/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,13 +21,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-
-    animationController =
-        AnimationController(vsync: this, duration: Duration(seconds: 1));
-    slidingAnimation = Tween<Offset>(begin: Offset(0, 5), end: Offset.zero)
-        .animate(animationController);
-
-    animationController.forward();
+    initAnimationController();
+    navigateToHomeView();
   }
 
   @override
@@ -40,10 +39,26 @@ class _SplashViewBodyState extends State<SplashViewBody>
         children: [
           Image.asset(
             AssetsData.logo,
+            fit: BoxFit.contain,
           ),
           SlidingText(slidingAnimation: slidingAnimation),
         ],
       ),
     );
+  }
+
+  void initAnimationController() {
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 5), end: Offset.zero)
+        .animate(animationController);
+
+    animationController.forward();
+  }
+  void navigateToHomeView() {
+     Future.delayed(Duration(seconds: 2), () {
+      Get.to(()=> const HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration);
+    });
   }
 }
