eadd67dfc167b184becf4124956ce46e67cb52b1
Abdulrahman <syamabdo382@gmail.com>
2025-12-26 00:25:19 +0200

    User GoRouter package for navigation instead of Get package

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,10 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -55,10 +55,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
     animationController.forward();
   }
+
   void navigateToHomeView() {
-     Future.delayed(Duration(seconds: 2), () {
-      Get.to(()=> const HomeView(),
-          transition: Transition.fade, duration: kTransitionDuration);
+    Future.delayed(Duration(seconds: 2), () {
+      // Get.to(()=> const HomeView(),
+      //     transition: Transition.fade, duration: kTransitionDuration);
+      GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
 }
