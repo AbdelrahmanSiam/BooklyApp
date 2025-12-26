6b74159ae47ebcb7f8f8f5ec0ae03a2bec626843
Abdulrahman <syamabdo382@gmail.com>
2025-12-26 04:42:51 +0200

    Add CustomScrollView

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -13,68 +13,75 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SafeArea(
-        // SafeArea build screen after default statless appBar
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              CustomBookDetailsViewAppBar(),
-              SizedBox(
-                height: 30,
+    return SafeArea(
+      // SafeArea build screen after default statless appBar
+      child: CustomScrollView( // to can use Expanded widgets inside it
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            CustomBookDetailsViewAppBar(),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .35,
+              child: CustomBookItem(
+                image:
+                    "https://img.freepik.com/free-vector/books-stack-realistic_1284-4735.jpg?semt=ais_hybrid&w=740&q=80",
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * .35,
-                child: CustomBookItem(
-                  image:
-                      "https://img.freepik.com/free-vector/books-stack-realistic_1284-4735.jpg?semt=ais_hybrid&w=740&q=80",
-                ),
-              ),
-              SizedBox(
-                height: 43,
-              ),
-              Text(
-                "The Jungle Book",
-                style: Styles.textSize30.copyWith(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 6,
-              ),
-              Text(
-                "Rudyard Kipling",
-                style: Styles.textSize16.copyWith(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              BookRatingRow(),
-              SizedBox(
+            ),
+            SizedBox(
+              height: 43,
+            ),
+            Text(
+              "The Jungle Book",
+              style: Styles.textSize30.copyWith(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Text(
+              "Rudyard Kipling",
+              style: Styles.textSize16.copyWith(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            BookRatingRow(),
+            SizedBox(
+              height: 37,
+            ),
+            BooksButton(),
+            Expanded(
+              child: SizedBox(
                 height: 37,
               ),
-              BooksButton(),
-              SizedBox(
-                height: 37,
+            ),
+            Align(
+              // to make only this text at the start
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "You can also read",
+                style: Styles.textSize22,
               ),
-              Align(
-                // to make only this text at the start
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "You can also read",
-                  style: Styles.textSize22,
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              BookDetailsListView(),
-              SizedBox(
-                height: 40,
-              ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            BookDetailsListView(),
+            SizedBox(
+              height: 40,
+            ),
+          ],
         ),
       ),
+          )
+        ],
+      )
     );
   }
 }
