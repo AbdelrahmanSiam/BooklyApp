0c5b10c4322ecb5abbb5b6c546052f74d360375d
Abdulrahman <syamabdo382@gmail.com>
2025-12-28 01:04:35 +0200

    create home repos

---
+++ lib/features/home/data/models/book_model/repoes/home_repo.dart
@@ -0,0 +1,5 @@
+abstract class HomeRepo {
+  // Good at collerbration teams ,Its methods can implemented in different ways , make it east to shift to Clean Arc
+  fetchFeaturedBooks();
+  fetchBestSellerBooks();
+}
