69ae0fd63970fff1b04d9d467fc1c2c294aa668f
Abdulrahman <syamabdo382@gmail.com>
2025-12-28 07:30:11 +0200

    create FeaturedCubit method

--- lib/features/home/presentation/manager/featured_cubit/featured_cubit_cubit.dart
+++ lib/features/home/presentation/manager/featured_cubit/featured_cubit_cubit.dart
@@ -1,15 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
-import 'package:bookly/features/home/data/repoes/home_repo_implementation.dart';
+import 'package:bookly/features/home/data/repoes/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_cubit_state.dart';
 
 class FeaturedCubitCubit extends Cubit<FeaturedCubitState> {
-  FeaturedCubitCubit() : super(FeaturedCubitInitial());
+  FeaturedCubitCubit(this.homeRepo) : super(FeaturedCubitInitial());
 
-  HomeRepoImplementation? homeRepoImplementation;
+  final HomeRepo homeRepo;
 
-  void getFeaturedBooksMethod() {
+  Future<void> getFeaturedBooksMethod() async {
+    emit(FeaturedCubitLoadingState());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure){
+      emit(FeaturedCubitFailureState(errMessage: failure.errorMessage));
+    }, (books){
+      emit(FeaturedCubitSuccesState(books: books));
+    });
   }
 }
