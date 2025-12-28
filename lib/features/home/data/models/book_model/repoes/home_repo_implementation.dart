b595ec3923ab9fd1e885f65dd92aa92320b17d7c
Abdulrahman <syamabdo382@gmail.com>
2025-12-28 06:59:11 +0200

    implement featured books method

--- lib/features/home/data/models/book_model/repoes/home_repo_implementation.dart
+++ lib/features/home/data/models/book_model/repoes/home_repo_implementation.dart
@@ -32,8 +32,23 @@ class HomeRepoImplementation implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoints:
+              "volumes?q=subject:Programming&Filtering=free-ebooks");
+      // data is map (items) that holds these endPoints specifically from the api, but i need list so we use for loop to loop on this map and get the lists(item)
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromBookJson(item)); // item = bookModel
+      }
+      return right(books); // Either<left, right >>
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(errorMessage: e.toString()));
+      }
+    }
   }
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -27,7 +27,7 @@ class HomeViewBody extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 30),
                 child: Text(
-                  "Best Seller",
+                  "Newest",
                   style: Styles.textSize22,
                 ),
               ),
